1174a86045eb4e846afe48318eaa5a0bfb4708cd
lis <lishang@icbf.com.cn>
2024-05-23 17:08:46 +0800

    feat(project): init project

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import AllFit from './views/allFit/allFit.vue'
+const activeNames = ref('all-fit')
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+    <!-- <TheWelcome /> -->
+    <sa-table-column />
+    <el-tabs v-model="activeNames" type="border-card">
+      <el-tab-pane label="全部自适应 (默认)" name="all-fit">
+        <div class="el-table">固定宽度者以宽度为准, 非固定宽度者自适应</div>
+        <all-fit />
+      </el-tab-pane>
+      <el-tab-pane label="部分不适应" name="part-not-fit">
+        <div class="el-table">设置 <code>&lt;af-table-column :fit="false"&gt;</code> 者不适应</div>
+      </el-tab-pane>
+      <el-tab-pane label="部分自适应" name="part-fit">
+        <div class="el-table">
+          设置 <code>&lt;el-table :autoFit="false"&gt;</code>后, 设置
+          <code>&lt;af-table-column fit&gt;</code> 者自适应
+        </div>
+      </el-tab-pane>
+    </el-tabs>
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/views/allFit/allFit.vue
@@ -0,0 +1,12 @@
+<template>
+  <el-table border>
+    <sa-table-column prop="date" label="编号" width="180" />
+    <el-table-column prop="date" label="日期" width="180"></el-table-column>
+    <el-table-column prop="name" label="姓名" width="180"></el-table-column>
+    <el-table-column prop="address" label="地址"></el-table-column>
+  </el-table>
+</template>
+<script setup lang="ts">
+defineOptions({ name: 'AllFit' })
+</script>
+<style></style>
